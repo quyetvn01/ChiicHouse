cb995e5b5de6172198c57166e416068cc54f4208
quyetvn01 <tranvanquyet010203@gmail.com>
2025-05-23 11:47:28 +0700

    Cập nhật giao diện và thêm icon Zalo

---
+++ client/js/index.js
@@ -0,0 +1,15 @@
+const swipers = new Swiper('.swiper', {           
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
